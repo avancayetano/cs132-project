91876a859091d989573f4492ce5bae387ec20b50
Harold Antonio <hcantonio1@up.edu.ph>
2023-02-28 16:32:51 +0800

    sidebar attempt

--- src/components/BasePage.js
+++ src/components/BasePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
+import BasePageSidebar from "./BasePageSidebar";
 
 const BasePage = (props) => {
   return (
     <>
-      <Navbar location={props.location} />
-      {props.children}
+      {/* <Navbar location={props.location} /> */}
+      <BasePageSidebar />
+      {/* {props.children} */}
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
     "./node_modules/flowbite-react/**/*.js",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
