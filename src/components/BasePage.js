24a531e77344f72d84bc2cc1720dabf24610d159
Anthony Van Cayetano <accayetano4@up.edu.ph>
2023-03-18 21:11:08 +0800

    Minor UI improvements

--- src/components/BasePage.js
+++ src/components/BasePage.js
@@ -7,7 +7,7 @@ const BasePage = (props) => {
       <input id="my-drawer" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
         <Navbar page={props.page} />
-        <div className="relative mb-16">{props.children}</div>
+        <div className="relative mb-16 min-h-screen">{props.children}</div>
         <Footer />
       </div>
       <div className="drawer-side">
