7605bcf169291b535530badef7f3c611e18a4ea3
Harold Antonio <hcantonio1@up.edu.ph>
2023-05-17 21:10:01 +0800

    nlp explained

--- src/pages/exploration.js
+++ src/pages/exploration.js
@@ -300,6 +300,30 @@ const ExplorationPage = () => {
                 <li>Stop words removal.</li>
                 <li>Stemming and lemmatization.</li>
               </ul>
+              <p>
+                The team wrote a code inspired by the natural language
+                processing notebook provided by the professor. First, emojis and
+                punctuation were replaced with their word counterparts. It can
+                be noted that a number of emojis and punctuations have
+                persisted. To address this, all the unaffected punctuation and
+                emojis were manually translated. The resulting text was casted
+                into lowercase.
+              </p>
+              <p>
+                Next, the team installed the googletrans api version 3.1.0a0.
+                This was the method used in translating the Tagalog tweets into
+                English. The team does not have a focus on NLP anyway. However,
+                there were empty tweets because such tweets were only images.
+                The error was finally caught and empty tweets translated into
+                empty strings.
+              </p>
+              <p>
+                Lastly, the tweets were tokenized using the nltk. The rest of
+                the professor's code has worked nicely. In the end, the
+                disinformation dataframe was appended the stemmed and lemmatized
+                version of the tweets. It should be noted that the tweets were
+                translated poorly by the automatic translator.
+              </p>
             </div>
             <div>
               <h3 id="time-series">Time Series Processing</h3>
